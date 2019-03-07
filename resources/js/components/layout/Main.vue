ea69ca63ffc7051111b7284794e70485d1f91970
Dave <vassdavid303@gmail.com>
2019-03-07 23:38:09 +0100

    Create Main Layout

---
+++ resources/js/components/layout/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div id="LeftSidebar" class="col-lg-3 col-md-4 bg-secondary fixed-top h-md-100">
+        <LeftSidebar />
+      </div>
+
+      <div id="content" class="offset-lg-3 offset-md-4 col-lg-9 col-md-8">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+</style>
