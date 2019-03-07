7b8c560720f08d27fa0ac0e77f3de2c8d2191793
Dave <vassdavid303@gmail.com>
2019-03-07 14:10:38 +0100

    Make Email model & migration

---
+++ app/Email.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model
+{
+  protected $table = 'emails';
+
+  /**
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+  protected $fillable = [
+    'email', 'user_id'
+  ];
+}
