c6718da1e40667518e3fc1115fdaef41ef19d870
Dave <vassdavid303@gmail.com>
2019-03-07 23:38:54 +0100

    Create Left Sidebar

---
+++ resources/js/components/LeftSidebar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav id="sidebar" class="navbar navbar-expand-md ">
+
+    <button
+      class="navbar-toggler navbar-toggler-right"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarVert"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse collapse flex-column " id="navbarVert">
+        <a class="nav-link navbar-brand active" href="#">Home</a>
+        <router-link class="nav-link" active-class="active" to="/users">Show Users</router-link>
+    </div>
+
+
+  </nav>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+.nav-link{
+  color: #ddd;
+  &:hover {
+    color: #efefef
+  }
+  &.active{
+    color: #00e4ff;
+  }
+}
+</style>
