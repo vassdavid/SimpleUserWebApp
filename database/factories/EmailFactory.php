850ea3adc11adc469960d7ac6563816fb08b2b51
Dave <vassdavid303@gmail.com>
2019-03-18 23:54:18 +0100

    Disable create user_id

--- database/factories/EmailFactory.php
+++ database/factories/EmailFactory.php
@@ -5,9 +5,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Email::class, function (Faker $faker) {
     return [
-        'email' => $faker->unique()->safeEmail,
-        'user_id' => function () {
-            return factory(App\User::class)->create()->id;
-        }
+        'email' => $faker->unique()->safeEmail
     ];
 });
