19f02d04b8b8aea89d1e0d55ed024ca672c21323
Dave <vassdavid303@gmail.com>
2019-09-26 22:06:08 +0200

    remove extract

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-// mix.sass('resources/sass/app.scss', 'public/css')
+mix.sass('resources/sass/app.scss', 'public/css')
 mix.js('resources/js/app.js', 'public/js')
-   .extract(['bootstrap','vue'])
-   .sourceMaps();
+   .sourceMaps()
